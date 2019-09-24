f1f175736b959390468ecef696bdd794ffd171bf
Tevinthuku <tevinthuku@gmail.com>
2019-09-25 01:23:33 +0300

    finalized build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 const withOffline = require("next-offline");
 
-const nextConfig = {};
+const nextConfig = {
+    exportPathMap: function() {
+      return {
+        '/': { page: '/' }
+      };
+    }
+  };
 
 module.exports = withOffline(nextConfig);
